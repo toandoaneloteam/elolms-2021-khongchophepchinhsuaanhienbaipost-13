dfc0b7cdf5b7fed7894fb4a76ff78d7229953e1a
toandoaneloteam <84503105+toandoaneloteam@users.noreply.github.com>
2021-05-24 16:40:07 +0700

    Add files via upload

---
+++ lang/vi/forum.php
@@ -0,0 +1,43 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'forum', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   mod_forum
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+//Elo: Duy
+$string['hide'] = 'Ẩn';
+$string['show'] = 'Hiện';
+$string['cannothidepost'] = 'Bạn không thể ẩn bài đăng này!';
+$string['cannothidepost'] = 'Bạn không thể hiện bài đăng này!';
+$string['hideddiscussion'] = 'Chủ đề thảo luận đã được ẩn';
+$string['hidedpost'] = 'Bài đăng đã được ẩn';
+$string['hidedposts'] = 'Những bài viết này đã được ẩn';
+$string['hidesure'] = 'Bạn có chắc chắn muốn ẩn bài đăng này không?';
+$string['showsure'] = 'Bạn có chắc chắn muốn hiện bài đăng này không?';
+$string['hidesureplural'] = 'Bạn có chắc chắn muốn ẩn bài đăng này và tất cả các câu trả lời không? Số bài đăng :{$a}';
+$string['showsureplural'] = 'Bạn có chắc chắn muốn hiển thị bài đăng này và tất cả các câu trả lời không? Số bài đăng :{$a}';
+$string['eventposthided'] = 'Bài đăng đã ẩn';
+$string['forumsubjecthided'] = 'Bài đăng này trên diễn đàn này đã bị ẩn';
+$string['forumbodyhided'] = 'Nội dung của bài đăng trên diễn đàn này đã bị ẩn và không thể truy cập được nữa.';
+$string['forum:hideanypost'] = 'Ẩn bất kỳ bài đăng nào (bất cứ lúc nào)';
+$string['forum:hideownpost'] = 'Ẩn bài viết của chính mình (trong thời hạn))';
+$string['errorwhilehide'] = 'Đã xảy ra lỗi khi cập nhật bản ghi.';
