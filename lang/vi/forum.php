c4b9a2c8ed1fc51064be2ab102a0eb414dcb2b55
toandoaneloteam <84503105+toandoaneloteam@users.noreply.github.com>
2021-05-25 09:50:15 +0700

    Update forum.php

--- lang/vi/forum.php
+++ lang/vi/forum.php
@@ -41,4 +41,5 @@ $string['forumbodyhided'] = 'Nội dung của bài đăng trên diễn đàn nà
 $string['forum:hideanypost'] = 'Ẩn bất kỳ bài đăng nào (bất cứ lúc nào)';
 $string['forum:hideownpost'] = 'Ẩn bài viết của chính mình (trong thời hạn))';
 $string['errorwhilehide'] = 'Đã xảy ra lỗi khi cập nhật bản ghi.';
+$string['eventdiscussionnorate'] = 'Tất cả thảo luận chưa chấm điểm';
 $string['anserforquestionasker'] = 'Thảo luận trả lời cho người đặt câu hỏi';
